dff05c2a59e8815139eba282ced2d3c1605f2ae4
Oliver Botello <47994633+Oliver404@users.noreply.github.com>
2022-07-26 09:54:22 -0500

    Example HMS Huawei Push Kit
    Update build.gradle

---
+++ app/src/main/java/com/oliverbotello/eha/pushkit/application/EHAPushKitApp.java
@@ -0,0 +1,20 @@
+package com.oliverbotello.eha.pushkit.application;
+
+import android.app.Application;
+
+import com.huawei.hms.push.HmsMessaging;
+
+public class EHAPushKitApp extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        setAutoInitEnabled(false);
+    }
+
+    /**
+     * @param isEnable
+     */
+    private void setAutoInitEnabled(final boolean isEnable) {
+        HmsMessaging.getInstance(this).setAutoInitEnabled(isEnable);
+    }
+}
